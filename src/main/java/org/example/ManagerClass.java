82a87306178884a89f9fb1ac4a5d964f7d8809dc
assiph <asif857@gmail.com>
2022-12-01 19:32:38 +0200

    added creds

--- src/main/java/org/example/ManagerClass.java
+++ src/main/java/org/example/ManagerClass.java
@@ -28,7 +28,7 @@ public class ManagerClass {
     final private String uploadBucket = "amazon-first-project";
     final private int workerToThreadRatio = 5;
 
-    final private String workerAmiId = "ami-071d02d4283d65a30";
+    final private String workerAmiId = "ami-06a5912e3695006d8";
 
 
     private List<Thread> threadList;
