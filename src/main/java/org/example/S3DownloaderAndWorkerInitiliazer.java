e4b2684ec3e210bb20ac47f4ac25c3ccfe4e3705
assiph <asif857@gmail.com>
2022-12-03 13:52:24 +0200

    added creds

--- src/main/java/org/example/S3DownloaderAndWorkerInitiliazer.java
+++ src/main/java/org/example/S3DownloaderAndWorkerInitiliazer.java
@@ -134,12 +134,12 @@ public class S3DownloaderAndWorkerInitiliazer implements Runnable{
         lines.add("echo Downloading Worker.jar");
         lines.add("zip -d Worker.jar 'META-INF/.SF' 'META-INF/.RSA' 'META-INF/*SF'");
         lines.add("echo Deleting Security Issues");
-        lines.add("java -jar Worker.jar");
-        lines.add("export LD_LIBRARY_PATH=/usr/local/lib");
+       // lines.add("java -jar Worker.jar");
+       // lines.add("export LD_LIBRARY_PATH=/usr/local/lib");
         //export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:usr/local/lib
-        lines.add("sudo ldconfig");
+        //lines.add("sudo ldconfig");
         lines.add("echo Setting Libraries");
-        lines.add("java -jar Worker.jar");
+      //  lines.add("java -jar Worker.jar");
         lines.add("echo Running Worker.jar");
         String str = Base64.getEncoder().encodeToString((join(lines, "\n").getBytes()));
         return str;
